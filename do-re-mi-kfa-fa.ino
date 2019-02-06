f66137f02dd58c7e5e3b8ade8a6b00a55e8bd476
ailaG <galia@galiaba.com>
2019-02-07 01:38:49 +0200

    Tidying up some code

--- do-re-mi-kfa-fa.ino
+++ do-re-mi-kfa-fa.ino
@@ -9,23 +9,21 @@
 
 #include "pitches.h"
 
-int max_piezo_inputs = 4;
-int iPIEZO_PINS[] = {A0, A1, A2, A3};
-//float iPIEZO_RANGE = 100;
-int iPIEZO_THRESHOLD = 10;
-int iPIEZO_THRESHOLDs[] = {50, 50, 50, 5} ;
-int oPIEZO_PIN = 9;
-int DIST_PIN = A5;
-int interval = 100;
+bool DEBUG = true;
+
+int num_fingers = 4;
+int FINGER_PINS[] = {A0, A1, A2, A3};
+int inputPiezoThreshold = 10;
+int DISTANCE_PIN = A5;
+int SPEAKER_PIN = 9;
 float outs1[] = {NOTE_C3, NOTE_E3, NOTE_G3, NOTE_C4};
 float outs2[] = {NOTE_D3, NOTE_G3, NOTE_AS3, NOTE_D4};
-//float outs1[] = {NOTE_C3, NOTE_E3, NOTE_C4};
-//float outs2[] = {NOTE_D3, NOTE_G3, NOTE_D4};
 
-boolean do_print = 1;
+int interval = 100;
+
 
 void setup() {
-  if (do_print) {
+  if (DEBUG) {
    Serial.begin(9600); 
   }
 } // setup
@@ -35,47 +33,48 @@ int tmpCount, outTone;
 
 void loop() {
   tmpCount =0;
-  outTone = 0;  
-  for (int i=0; i<max_piezo_inputs; i++) {
-    tmpOut = analogRead(iPIEZO_PINS[i]);
-    if (do_print) {
+  outTone = 0;
+
+  // Read fingers
+  for (int i=0; i<num_fingers; i++) {
+    tmpOut = analogRead(FINGER_PINS[i]);
+    if (DEBUG) {
       Serial.print(tmpOut);
       Serial.print(" dist=" );
-      Serial.print(digitalRead(DIST_PIN));
+      Serial.print(digitalRead(DISTANCE_PIN));
       Serial.print(" ");
     }
-    if (tmpOut < iPIEZO_THRESHOLD) {
-      Serial.print("AA"); 
-      if (digitalRead(DIST_PIN) == 1) {
+    if (tmpOut < inputPiezoThreshold) {
+      if (digitalRead(DISTANCE_PIN) == 1) {
         outTone += outs1[i];
       } else {
         outTone += outs2[i];
       }
       tmpCount++;
-      if (do_print) {
+      if (DEBUG) {
         Serial.print(outTone);
       }
-      //noTone(oPIEZO_PIN);
     } else {
-      if (do_print) {
+      if (DEBUG) {
         Serial.print("--.--"); 
       }
     }
         
-    if (do_print) {
+    if (DEBUG) {
       Serial.print(" OUT : ");
       Serial.print(outTone);
       Serial.print(" | ");
     }
-  } // for loop
-  
+  } // finger checking loop
+
+  // Play music
   if (outTone > 0) {
-    tone(oPIEZO_PIN, outTone / tmpCount);
+    tone(SPEAKER_PIN, outTone / tmpCount);
     delay(interval);
   } else {
-    noTone(oPIEZO_PIN);
+    noTone(SPEAKER_PIN);
   }
-  if (do_print) {
+  if (DEBUG) {
     Serial.println("");
   }
 } // loop
