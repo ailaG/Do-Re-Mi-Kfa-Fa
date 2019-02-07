8aa0dc6634bb309118bb78b906a4925321776c8a
ailaG <galia@galiaba.com>
2019-02-07 04:52:54 +0200

    Changed threshold (see next commit, too). More tidying and debug messages.

--- do-re-mi-kfa-fa.ino
+++ do-re-mi-kfa-fa.ino
@@ -11,15 +11,15 @@
 
 bool DEBUG = true;
 
-int num_fingers = 4;
 int FINGER_PINS[] = {A0, A1, A2, A3};
-int inputPiezoThreshold = 10;
 int DISTANCE_PIN = A5;
 int SPEAKER_PIN = 9;
 
 float notes_regular[] = {NOTE_C3, NOTE_E3, NOTE_G3, NOTE_C4};
 float notes_special[] = {NOTE_D3, NOTE_G3, NOTE_AS3, NOTE_D4};
 
+int num_fingers = 4;
+int inputPiezoThreshold = 500;
 int interval = 100;
 
 
@@ -40,16 +40,16 @@ int tmpCount, outTone;
 
 void loop() {
   tmpCount =0;
+  tmpOut = 0;
   outTone = 0;
 
   // Read fingers
   for (int i=0; i<num_fingers; i++) {
     tmpOut = analogRead(FINGER_PINS[i]);
     if (DEBUG) {
+      Serial.print(i);
+      Serial.print(": ");
       Serial.print(tmpOut);
-      Serial.print(" dist=" );
-      Serial.print(digitalRead(DISTANCE_PIN));
-      Serial.print(" ");
     }
     if (tmpOut < inputPiezoThreshold) {
       // Finger #i pressed!
@@ -59,32 +59,34 @@ void loop() {
       } else {
         outTone += notes_special[i];
       }
+      
       tmpCount++;
       
       if (DEBUG) {
-        Serial.print("Pressed: ");
-        Serial.print(i);
-        Serial.print("outTone: ");
+        Serial.print(" Pressed! ");
+        Serial.print(" outTone: ");
         Serial.print(outTone);
       }
     } else {
       // Not pressed hard enough
       if (DEBUG) {
-        Serial.print("--.--"); 
+        Serial.print(" --------- "); 
+        Serial.print(" outTone: ");
+        Serial.print(outTone);
       }
     }
         
-    if (DEBUG) {
-      Serial.print(" OUT : ");
-      Serial.print(outTone);
-      Serial.print(" | ");
-    }
-  } // finger checking loop
+  } // per-finger loop
 
-  // Play music
+
+  // Play music!
   if (outTone > 0) {
     tone(SPEAKER_PIN, outTone / tmpCount);
     delay(interval);
+    if (DEBUG) {
+      Serial.print(" TOTAL OUT : ");
+      Serial.print(outTone);
+    }
   } else {
     noTone(SPEAKER_PIN);
   }
